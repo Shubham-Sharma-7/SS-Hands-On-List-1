3eb0715353b00bad7bafbedb79f402d8383cadab
Shubham Sharma <Shubham.Sharma@iiitb.ac.in>
2024-09-09 18:05:02 +0530

    Update 5.c

--- 5.c
+++ 5.c
@@ -63,5 +63,17 @@ fd =  5
 fd =  6
 fd =  7
 then click CTRL + D to stop
+(base) mohitsharma@MacBook-Pro copy for hands for list 1 % lsof -p 3334
+COMMAND  PID        USER   FD   TYPE DEVICE  SIZE/OFF    NODE NAME
+5c      3334 mohitsharma  cwd    DIR   1,13      3520 5205370 /Users/mohitsharma/copy for hands for list 1
+5c      3334 mohitsharma  txt    REG   1,13     33552 5420381 /Users/mohitsharma/copy for hands for list 1/5c
+5c      3334 mohitsharma    0u   CHR   16,0 0t1856753     699 /dev/ttys000
+5c      3334 mohitsharma    1u   CHR   16,0 0t1856753     699 /dev/ttys000
+5c      3334 mohitsharma    2u   CHR   16,0 0t1856753     699 /dev/ttys000
+5c      3334 mohitsharma    3r   REG   1,13         0 5214227 /Users/mohitsharma/copy for hands for list 1/foo.txt
+5c      3334 mohitsharma    4r   REG   1,13         0 5214227 /Users/mohitsharma/copy for hands for list 1/foo.txt
+5c      3334 mohitsharma    5r   REG   1,13         0 5214227 /Users/mohitsharma/copy for hands for list 1/foo.txt
+5c      3334 mohitsharma    6r   REG   1,13         0 5214227 /Users/mohitsharma/copy for hands for list 1/foo.txt
+5c      3334 mohitsharma    7r   REG   1,13         0 5214227 /Users/mohitsharma/copy for hands for list 1/foo.txt
 ============================================================================
 */
