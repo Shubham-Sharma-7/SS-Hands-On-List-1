b341f4fbdfdf9504ef1cdc114bbb52467cbf326e
Shubham Sharma <Shubham.Sharma@iiitb.ac.in>
2024-09-14 13:47:24 +0530

    23.c

--- 23.c
+++ 23.c
@@ -7,30 +7,26 @@ Date: 27th Aug, 2024.
 ============================================================================
 */
 #include <stdio.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/resource.h>
-#include <unistd.h>
 #include <stdlib.h>
-#include <sys/wait.h>
-int main(){
+#include <unistd.h>
+
+int main() {
     pid_t pid;
-    pid=fork();
-    int abc;
-    if(pid<0){
-        printf("forkfailed\n");
+    pid = fork();
+    if (pid < 0) {
+        perror("fork failed");
         exit(1);
-    }
-    if(pid==0){
-        sleep(10);
-        printf("In child\n");
-    }else{
-        abc=getpid();
-        printf("In parent\n");
+    } else if (pid == 0) {
+        printf("Child process with PID %d is exiting.\n", getpid());
         exit(0);
+    } else {
+        printf("Parent process with PID %d created a child process with PID %d.\n", getpid(), pid);
+        sleep(10);
+        printf("Parent process is now exiting.\n");
     }
     return 0;
 }
+
  /*
 ============================================================================
 (base) mohitsharma@MacBook-Pro hands_on_list_1 % gcc 23.c -o 23
